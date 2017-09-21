6177536d1f1909bc4d28e2d8a40d14f9cd4ba98c
Dan Piston <danpiston@gmail.com>
2017-09-20 23:11:10 -0400

    Lock everything behind login
    Locked all behind login with Google
    Extend catalog.html to all templates

--- application.py
+++ application.py
@@ -23,13 +23,16 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+# Categories variable to pass to templates
+
 #Login informstion for session
 @app.route('/login')
 def showLogin():
+    categories = session.query(Category).all()
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in range(32))
     login_session['state'] = state
-    return render_template('login.html', STATE=state)
+    return render_template('login.html', STATE=state,categories=categories)
 
 #
 @app.route('/gconnect', methods=['POST'])
@@ -186,93 +189,119 @@ def showCatalog():
 #Create new category
 @app.route('/category/new/', methods=['GET', 'POST'])
 def newCategory():
-    if request.method == 'POST':
-        newCategory = Category(name = request.form['name'])
-        session.add(newCategory)
-        session.commit()
-        return redirect(url_for('showCatalog'))
-    return render_template('newcategory.html')
+    categories = session.query(Category).all()
+    if 'username' in login_session:
+        if request.method == 'POST':
+            newCategory = Category(name = request.form['name'])
+            session.add(newCategory)
+            session.commit()
+            return redirect(url_for('showCatalog'))
+        return render_template('newcategory.html', categories=categories, login_session=login_session)
+    else:
+        return redirect(url_for('showLogin'))
 
 #Edit category
 @app.route('/category/<int:category_id>/edit/', methods=['POST', 'GET'])
 def editCategory(category_id):
+    categories = session.query(Category).all()
     editedCat = session.query(Category).filter_by(id=category_id).one()
-    if request.method == 'POST':
-        editedCat.name = request.form['name']
-        session.add(editedCat)
-        session.commit()
-        return redirect(url_for('showCatalog'))
+    if 'username' in login_session:
+        if request.method == 'POST':
+            editedCat.name = request.form['name']
+            session.add(editedCat)
+            session.commit()
+            return redirect(url_for('showCatalog'))
+        else:
+            return render_template('editcategory.html', category_id=category_id, category=editedCat, categories=categories)
+        return "Edit category {} here".format(category_id)
     else:
-        return render_template('editcategory.html', category_id=category_id, category=editedCat)
-    return "Edit category {} here".format(category_id)
+        return redirect(url_for('showLogin'))
 
 #Delete category
 @app.route('/category/<int:category_id>/delete/', methods=['GET', 'POST'])
 def deleteCategory(category_id):
+    categories = session.query(Category).all()
     deleteItems = session.query(Item).filter_by(category_id=category_id).all()
     deletedCat = session.query(Category).filter_by(id=category_id).one()
-    if request.method == "POST":
-        #remove items in category as well
-        for item in deleteItems:
-            session.delete(item)
-        session.delete(deletedCat)
-        session.commit()
-        return redirect(url_for('showCatalog'))
+    if 'username' in login_session:
+        if request.method == "POST":
+            #remove items in category as well
+            for item in deleteItems:
+                session.delete(item)
+            session.delete(deletedCat)
+            session.commit()
+            return redirect(url_for('showCatalog'))
+        else:
+            return render_template('deletecategory.html', category_id=category_id, category = deletedCat, categories=categories)
     else:
-        return render_template('deletecategory.html', category_id=category_id, category = deletedCat)
+        return redirect(url_for('showLogin'))
 
 #Display category
 @app.route('/category/<int:category_id>/')
 def showCategory(category_id):
+    categories = session.query(Category).all()
     category = session.query(Category).filter_by(id=category_id).one()
     items = session.query(Item).filter_by(category_id=category_id)
-    return render_template('showcategory.html', category=category, items=items)
+    return render_template('showcategory.html', category=category, items=items, categories=categories, login_session=login_session)
 
 #Create new item
 @app.route('/category/<int:category_id>/item/new/', methods=['GET', 'POST'])
 def newItem(category_id):
-    if request.method == "POST":
-        newItem = Item(name = request.form['name'], description = request.form['description'], category_id=category_id)
-        session.add(newItem)
-        session.commit()
-        return redirect(url_for('showCategory', category_id=category_id))
-    else:
-        return render_template('newitem.html', category_id=category_id)
+    categories = session.query(Category).all()
+    if 'username' in login_session:
+        if request.method == "POST":
+            newItem = Item(name = request.form['name'], description = request.form['description'], category_id=category_id)
+            session.add(newItem)
+            session.commit()
+            return redirect(url_for('showCategory', category_id=category_id))
+        else:
+            return render_template('newitem.html', category_id=category_id,categories=categories, login_session=login_session)
 
-    return 'Create new item here'
-    app.secret_key = 'super_secret_key'
+        return 'Create new item here'
+        app.secret_key = 'super_secret_key'
+    else:
+        return redirect(url_for('showLogin'))
 
 #Edit item
 @app.route('/category/<int:category_id>/item/<int:item_id>/edit', methods=['POST', 'GET'])
 def editItem(item_id, category_id):
+    categories = session.query(Category).all()
     editedItem = session.query(Item).filter_by(id=item_id).one()
-    if request.method == "POST":
-        if request.form['name']:
-            editedItem.name = request.form['name']
-        if request.form['description']:
-            editedItem.description=request.form['description']
-        session.add(editedItem)
-        session.commit()
-        return redirect(url_for('showCategory', category_id=category_id))
+    if 'username' in login_session:
+        if request.method == "POST":
+            if request.form['name']:
+                editedItem.name = request.form['name']
+            if request.form['description']:
+                editedItem.description=request.form['description']
+            session.add(editedItem)
+            session.commit()
+            return redirect(url_for('showCategory', category_id=category_id))
+        else:
+            return render_template('edititem.html', category_id=category_id, item=editedItem, categories=categories, login_session=login_session)
     else:
-        return render_template('edititem.html', category_id=category_id, item=editedItem)
+        return redirect(url_for('showLogin'))
 
 #Delete item
 @app.route('/category/<int:category_id>/item/<int:item_id>/delete', methods=['POST', 'GET'])
 def deleteItem(item_id, category_id):
     deletedItem = session.query(Item).filter_by(id=item_id).one()
-    if request.method == "POST":
-        session.delete(deletedItem)
-        session.commit()
-        return redirect(url_for('showCategory', category_id=category_id))
+    categories = session.query(Category).all()
+    if 'username' in login_session:
+        if request.method == "POST":
+            session.delete(deletedItem)
+            session.commit()
+            return redirect(url_for('showCategory', category_id=category_id))
+        else:
+            return render_template('deleteitem.html', category_id=category_id, item=deletedItem, categories=categories, login_session=login_session)
     else:
-        return render_template('deleteitem.html', category_id=category_id, item=deletedItem)
+        return redirect(url_for('showLogin'))
 
 #Show item
 @app.route('/category/<int:category_id>/item/<int:item_id>/')
 def showItem(item_id, category_id):
+    categories = session.query(Category).all()
     item = session.query(Item).filter_by(id=item_id).one()
-    return render_template('showitem.html', category_id=category_id, item=item)
+    return render_template('showitem.html', category_id=category_id, item=item, categories=categories, login_session=login_session)
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
